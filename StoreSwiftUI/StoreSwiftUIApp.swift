5effce9633394619bcc83f3a2cdee3fc87bc46a2
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-19 22:16:51 +0500

    Initial Commit

---
+++ StoreSwiftUI/StoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreSwiftUIApp.swift
+//  StoreSwiftUI
+//
+//  Created by sss on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
